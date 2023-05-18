03fe2bf7097c55a1503b967edfcdc4dba2a3f1e5
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-18 13:27:57 +0530

    8

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({id, img, title, desc, price}) => {
+  return (
+    <div className='menu'> 
+      <img src={img} />
+      <div className='menu-block-header'>
+      <h3>{title}</h3><span>${price}</span>
+      </div>
+      <p>{desc}</p>
+    </div>
+  );
 };
 
 export default Menu;
