9327fad30f6787b69b9eb3031bd0c7b1cb2e5f52
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-18 14:57:37 +0530

    app code final

--- src/App.js
+++ src/App.js
@@ -6,42 +6,52 @@ import items from './data';
 const allCategories = ['All', ...new Set(items.map(item => item.category))];
 
 function App() {
+
   const [menu, setMenu] = useState(items);
   const [searchValue, setSearchValue] = useState('');
   const [categories, setCategories] = useState(allCategories);
   useEffect(()=>{
-    if(searchValue.length === 0) {
+    if(searchValue.length === 0 && categories) {
       setMenu(items);
     }
-  });
+  },[searchValue]);
+
+
 
   const clear = () => {
     setSearchValue('');
     setMenu(items);
   }
+
   const search = (event) => {
       setSearchValue(event.target.value);
-      const newMenu = items.filter(item => {
-        return item.title.includes(searchValue.toLowerCase().trim()) || item.category.includes(searchValue.toLowerCase().trim());
-      })
-      setMenu(newMenu);
+      searchHandler(searchValue);
   }
+
+  const searchHandler = (searchValue) => {
+    const newMenu = items.filter(item => {
+      return item.title.includes(searchValue.toLowerCase().trim()) || item.category.includes(searchValue.toLowerCase().trim());
+    })
+    setMenu(newMenu);
+  }
+
   const filterCategories = (category) => {
     if(category === 'All') {
       setMenu(items);
       return;
     }
-
     const newItem = items.filter(item => item.category === category);
     setMenu(newItem);
     return;
   }
   
+
+
   return (
     <main>
     <div className='section'>
       <h2>We Serve</h2>
-      <p>Savor the Flavor: Where Culinary Delights Come Alive!</p>
+      <p className='error-message'>Savor the Flavor: Where Culinary Delights Come Alive!</p>
       <Search search={search} clear={clear} searchValue={searchValue}/>
       <div className='categories'>
         <Categories categories={categories} filterCategories={filterCategories}/>
