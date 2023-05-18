54a40594769e7be28cbbf7822742dac903371124
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-18 13:27:46 +0530

    4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
+import Search from './Search';
 import items from './data';
+const allCategories = ['All', ...new Set(items.map(item => item.category))];
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const search = (event) => {
+      const searchValue = event.target.value.trim();
+      if(searchValue === ''){
+        setMenu(items);
+        return;
+      }
+      const newMenu = menu.filter(item => {
+        return item.title.includes(searchValue.toLowerCase()) || item.category.includes(searchValue.toLowerCase());
+      })
+      setMenu(newMenu);
+  }
+  const filterCategories = (category) => {
+    if(category === 'All') {
+      setMenu(items);
+      return;
+    }
+
+    const newItem = items.filter(item => item.category === category);
+    setMenu(newItem);
+    return;
+  }
+  
+  return (
+    <main>
+    <div className='section'>
+      <h2>We Serve</h2>
+      <p>Savor the Flavor: Where Culinary Delights Come Alive!</p>
+      <Search search={search} />
+      <div className='categories'>
+        <Categories categories={categories} filterCategories={filterCategories}/>
+      </div>
+      <div className='menu-section'>
+        {menu.length === 0 ? <p>OPPS! we dont serve that here</p> : ''}
+        {menu.map(item => <Menu key={item.id} {...item}/>)}
+      </div>
+    </div>
+    </main>
+  );
 }
 
 export default App;
